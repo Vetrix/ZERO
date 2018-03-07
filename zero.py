8df8a0c0dea17cfc2a376c0f3e714c1622c0e969
Vetrix <34627197+Vetrix@users.noreply.github.com>
2018-03-07 11:16:46 +0700

    Add files via upload

--- zero.py
+++ zero.py
@@ -47,8 +47,8 @@ def handle_message(event):
 		# WolframAlpha AppID, obtained from developer.wolframalpha.com
 		wolfram_appid = ('83L4JP-TWUV8VV7J7')
 
-		url = 'https://api.wolframalpha.com/v1/{}?i={}&appid={}'
-		return requests.get(url.format(true, quote(query), wolfram_appid))
+		url = 'https://api.wolframalpha.com/v2/query?input={}&appid={}'
+		return requests.get(url.format(quote(query), wolfram_appid))
 	
 	if text == '/help':
 		line_bot_api.reply_message(
