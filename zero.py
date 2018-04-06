59f47b636d538332ee465aaa5522a3974087a55d
Dzannun Austi <34627197+Vetrix@users.noreply.github.com>
2018-04-06 18:09:01 +0700

    added import

--- zero.py
+++ zero.py
@@ -16,6 +16,13 @@ from linebot.exceptions import (
 )
 from linebot.models import (
 	MessageEvent, TextMessage, TextSendMessage, SourceGroup, SourceRoom
+	TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+	ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URITemplateAction,
+	PostbackTemplateAction, DatetimePickerTemplateAction,
+	CarouselTemplate, CarouselColumn, PostbackEvent,
+	StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+	ImageMessage, VideoMessage, AudioMessage, FileMessage,
+	UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 
 app = Flask(__name__)
