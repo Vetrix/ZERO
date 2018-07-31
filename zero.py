0b23b6434b5697c68d4508d9e1cad3a876c6e8e1
Dzannun Austi <34627197+Vetrix@users.noreply.github.com>
2018-07-31 22:41:36 +0700

    try tmp

--- zero.py
+++ zero.py
@@ -599,5 +599,14 @@ def handle_beacon(event):
 				event.beacon.hwid, event.beacon.dm)))
 	
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+	arg_parser = ArgumentParser(
+		usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+	)
+	arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+	arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+	options = arg_parser.parse_args()
+
+	# create tmp dir for download content
+	make_static_tmp_dir()
+
+	app.run(debug=options.debug, port=options.port)
