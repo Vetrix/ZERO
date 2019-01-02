e23bb6168392629927b10c786272ec00bae11c00
Dzannun Austi <34627197+Vetrix@users.noreply.github.com>
2019-01-02 20:12:21 +0700

    cek

--- zero.py
+++ zero.py
@@ -451,8 +451,9 @@ def handle_text_message(event):
 				TextSendMessage("Without parameters: \n"
 								"/about, /help, /profile, /leave, /lang \n"
 								"/confirm, /buttons, /search image, \n"
-								"/manga, /carousel, /flex, \n"
-								"/image_carousel, /quick \n"
+								"/image_carousel\n"
+								"/manga\n" #/quick. /carousel, /flex
+								
 								"\n"
 								"With parameters: \n"
 								"/echo, /kbbi, /wolfram, /wolframs, \n"
@@ -478,22 +479,7 @@ def handle_text_message(event):
 								"https://reverse.photos/"))
 	
 	elif text == '/profile':
-		if isinstance(event.source, SourceUser):
-			profile = line_bot_api.get_profile(event.source.user_id)
-			result = ("Display name: " + profile.display_name + "\n" +
-						  "Profile picture: " + profile.picture_url + "\n" +
-						  "User_ID: " + profile.user_id)
-			result += "\n" + "Status message: " + profile.status_message
-			
-			line_bot_api.reply_message(
-				event.reply_token, [
-					TextSendMessage(result)])
-		else:
-			line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text="Bot can't use profile API without user ID"))
-
-		"""if isinstance(event.source, SourceGroup):
+		if isinstance(event.source, SourceGroup):
 			try:
 				profile = line_bot_api.get_group_member_profile(event.source.group_id, event.source.user_id)
 				result = ("Display name: " + profile.display_name + "\n" +
@@ -519,7 +505,7 @@ def handle_text_message(event):
 				TextSendMessage(result))
 			
 				
-		else:
+		elif isinstance(event.source, SourceUser):
 			try:
 				profile = line_bot_api.get_profile(event.source.user_id)
 				result = ("Display name: " + profile.display_name + "\n" +
@@ -531,7 +517,12 @@ def handle_text_message(event):
 				pass
 			line_bot_api.reply_message(
 				event.reply_token,
-				TextSendMessage(result))"""
+				TextSendMessage(result))
+
+		else:
+			line_bot_api.reply_message(
+			event.reply_token,
+			TextSendMessage(text="Bot can't use profile API without user ID"))
 	
 	elif text=='/kbbi':
 		line_bot_api.reply_message(
@@ -651,22 +642,7 @@ def handle_text_message(event):
 		template_message = TemplateSendMessage(
 			alt_text='Buttons alt text', template=buttons_template)
 		line_bot_api.reply_message(event.reply_token, template_message)
-	
-	elif text == '/carousel':
-		carousel_template = CarouselTemplate(columns=[
-			CarouselColumn(text='hoge1', title='fuga1', actions=[
-				URIAction(label='Go to line.me', uri='https://line.me'),
-				PostbackAction(label='ping', data='ping')
-			]),
-			CarouselColumn(text='hoge2', title='fuga2', actions=[
-				PostbackAction(label='ping with text', data='ping', text='ping'),
-				MessageAction(label='Translate Rice', text='米')
-			]),
-		])
-		template_message = TemplateSendMessage(
-			alt_text='Carousel alt text', template=carousel_template)
-		line_bot_api.reply_message(event.reply_token, template_message)
-	
+
 	elif text == '/image_carousel':
 		image_carousel_template = ImageCarouselTemplate(columns=[
 			ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
@@ -684,142 +660,7 @@ def handle_text_message(event):
 		
 	elif text == '/imagemap':
 		pass
-	
-	elif text == '/flex':
-		bubble = BubbleContainer(
-			direction='ltr',
-			hero=ImageComponent(
-				url='https://example.com/cafe.jpg',
-				size='full',
-				aspect_ratio='20:13',
-				aspect_mode='cover',
-				action=URIAction(uri='http://example.com', label='label')
-			),
-			body=BoxComponent(
-				layout='vertical',
-				contents=[
-					# title
-					TextComponent(text='Brown Cafe', weight='bold', size='xl'),
-					# review
-					BoxComponent(
-						layout='baseline',
-						margin='md',
-						contents=[
-							IconComponent(size='sm', url='https://example.com/gold_star.png'),
-							IconComponent(size='sm', url='https://example.com/grey_star.png'),
-							IconComponent(size='sm', url='https://example.com/gold_star.png'),
-							IconComponent(size='sm', url='https://example.com/gold_star.png'),
-							IconComponent(size='sm', url='https://example.com/grey_star.png'),
-							TextComponent(text='4.0', size='sm', color='#999999', margin='md',
-										  flex=0)
-						]
-					),
-					# info
-					BoxComponent(
-						layout='vertical',
-						margin='lg',
-						spacing='sm',
-						contents=[
-							BoxComponent(
-								layout='baseline',
-								spacing='sm',
-								contents=[
-									TextComponent(
-										text='Place',
-										color='#aaaaaa',
-										size='sm',
-										flex=1
-									),
-									TextComponent(
-										text='Shinjuku, Tokyo',
-										wrap=True,
-										color='#666666',
-										size='sm',
-										flex=5
-									)
-								],
-							),
-							BoxComponent(
-								layout='baseline',
-								spacing='sm',
-								contents=[
-									TextComponent(
-										text='Time',
-										color='#aaaaaa',
-										size='sm',
-										flex=1
-									),
-									TextComponent(
-										text="10:00 - 23:00",
-										wrap=True,
-										color='#666666',
-										size='sm',
-										flex=5,
-									),
-								],
-							),
-						],
-					)
-				],
-			),
-			footer=BoxComponent(
-				layout='vertical',
-				spacing='sm',
-				contents=[
-					# callAction, separator, websiteAction
-					SpacerComponent(size='sm'),
-					# callAction
-					ButtonComponent(
-						style='link',
-						height='sm',
-						action=URIAction(label='CALL', uri='tel:000000'),
-					),
-					# separator
-					SeparatorComponent(),
-					# websiteAction
-					ButtonComponent(
-						style='link',
-						height='sm',
-						action=URIAction(label='WEBSITE', uri="https://example.com")
-					)
-				]
-			),
-		)
-		message = FlexSendMessage(alt_text="hello", contents=bubble)
-		line_bot_api.reply_message(
-			event.reply_token,
-			message
-		)
-	
-	elif text == '/quick':
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(
-				text='Quick reply',
-				quick_reply=QuickReply(
-					items=[
-						QuickReplyButton(
-							action=PostbackAction(label="label1", data="data1")
-						),
-						QuickReplyButton(
-							action=MessageAction(label="label2", text="text2")
-						),
-						QuickReplyButton(
-							action=DatetimePickerAction(label="label3",
-														data="data3",
-														mode="date")
-						),
-						QuickReplyButton(
-							action=CameraAction(label="label4")
-						),
-						QuickReplyButton(
-							action=CameraRollAction(label="label5")
-						),
-						QuickReplyButton(
-							action=LocationAction(label="label6")
-						),
-					])))	
-	
+		
 	elif text[0:].lower().strip().startswith('/videoig '):
 		line_bot_api.reply_message(
 			event.reply_token, [
