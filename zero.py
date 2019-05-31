a8ed0ad957253d62f61271ad78e315bae6fb4fe2
Dzannun Austi <34627197+Vetrix@users.noreply.github.com>
2019-05-31 19:09:15 +0700

    generalize

--- zero.py
+++ zero.py
@@ -23,8 +23,9 @@ save_file = {}
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('CQcg1+DqDmLr8bouXAsuoSm5vuwB2DzDXpWc/KGUlxzhq9MSWbk9gRFbanmFTbv9wwW8psPOrrg+mHtOkp1l+CTlqVeoUWwWfo54lNh16CcqH7wmQQHT+KnkNataGXez6nNY8YlahgO7piAAKqfjLgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('c116ac1004040f97a62aa9c3503d52d9')
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+## https://developers.line.biz/en/ your official account
 
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 	
@@ -126,10 +127,11 @@ def handle_text_message(event):
 	
 	def fdetect(url) :
 		http_url = 'https://api-us.faceplusplus.com/facepp/v3/detect'
-		key = "lM4feUDrJJYyKm8s4WvmxgWlVpZE0iNw"
-		secret = "Y5L_l2a87UAtKeFzL-FD8K3C5OwtAbfA"
+		key = "YOUR_FACE++_KEY"
+		secret = "YOUR_FACE++_SECRET"
 		attributes="age,gender,ethnicity,beauty"
 		
+		##more info of thr api on https://api-us.faceplusplus.com
 		#add emotion later in atributes
 		
 		resp = requests.post(http_url,
@@ -188,11 +190,13 @@ def handle_text_message(event):
 		query = query.split('/', 5)[-1]
 		query = query.split('/')[0]
 		
-		access_token = ('1084471906248445952-3aN04mwGh4Y3VKUXFVV1YIfiC6WPVB')
-		access_token_secret = ('XsRaIf04Te3AldW5mRFz1kgaT1ZyyXWNaDOlK1GKivDUE')
-		consumer_key = ('u1CBf3TrAu1BDp7qsAH2GbSwl')
-		consumer_secret = ('P2vMIp4vSuM7Q1PdscyjAE8eMcoROCIBY5mZ4H5WHQXPxWbcwy')
+		access_token = ('YOUR_TWITTER_API_ACCESS_TOKEN')
+		access_token_secret = ('YOUR_TWITTER_API_ACCESS_TOKEN_SECRET')
+		consumer_key = ('YOUR_TWITTER_API_CONSUMER_KEY')
+		consumer_secret = ('YOUR_TWITTER_API_CONSUMER_SECRET')
 		
+	##more info of thr api on https://developer.twitter.com
+	
 		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 		auth.set_access_token(access_token, access_token_secret)
 
@@ -361,9 +365,9 @@ def handle_text_message(event):
 		return (str(data['cover url']))
 	
 	def ox(keyword):
-		oxdict_appid = ('7dff6c56')
-		oxdict_key = ('41b55bba54078e9fb9f587f1b978121f')
-		
+		oxdict_appid = ('YOUR_OXFORDDICTIONARIES_APPID')
+		oxdict_key = ('YOUR_OXFORDDICTIONARIES_KEY')
+	##more info of thr api on https://od-api.oxforddictionaries.com
 		word = quote(keyword)
 		url = ('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/{}'.format(word))
 		req = requests.get(url, headers={'app_id': oxdict_appid, 'app_key': oxdict_key})
@@ -393,13 +397,13 @@ def handle_text_message(event):
 
 	
 	def wolfram(query):
-		wolfram_appid = ('83L4JP-TWUV8VV7J7')
+		wolfram_appid = ('YOUR_WOLFRAMALPHA_APPID')
 
 		url = 'https://api.wolframalpha.com/v2/result?i={}&appid={}'
 		return requests.get(url.format(quote(query), wolfram_appid)).text
 		
 	def wolframs(query):
-		wolfram_appid = ('83L4JP-TWUV8VV7J7')
+		wolfram_appid = ('YOUR_WOLFRAMALPHA_APPID')
 
 		url = 'https://api.wolframalpha.com/v2/simple?i={}&appid={}'
 		return url.format(quote(query), wolfram_appid)
